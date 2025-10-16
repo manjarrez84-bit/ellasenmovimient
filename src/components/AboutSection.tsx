5f7678c719bcd6a9723a8a82c8329532f30dfc4a
[dyad] <git@dyad.sh>
2025-10-15 20:11:23 -0600

    [dyad] Agregando filtro de blanco y negro y más transparencia a la foto de fondo - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -16,11 +16,11 @@ const AboutSection = () => {
           <Card className="p-6 shadow-lg relative overflow-hidden dark:bg-gray-700 dark:border-gray-600">
             {/* Background Image */}
             <div
-              className="absolute inset-0 bg-cover bg-center"
+              className="absolute inset-0 bg-cover bg-center grayscale" // Añadido 'grayscale' aquí
               style={{ backgroundImage: "url('/mission-background.jpg')" }}
             ></div>
             {/* Overlay for readability */}
-            <div className="absolute inset-0 bg-black opacity-20"></div>
+            <div className="absolute inset-0 bg-black opacity-10"></div> {/* Opacidad reducida a 10 */}
 
             <CardHeader className="flex flex-col items-center relative z-10">
               <Goal className="text-white mb-4" size={40} />
