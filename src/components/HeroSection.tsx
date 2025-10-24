46c05239e9f0f4bf76f439c185f27c67fa4f8331
[dyad] <manjarrez.84@gmail.com>
2025-10-24 09:50:00 -0600

    [dyad] Updated hero section background image - wrote 1 file(s)

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   return (
     <section 
       className="relative h-[75vh] flex flex-col items-center justify-center text-center text-white pt-32 bg-cover bg-top"
-      style={{ backgroundImage: "url('/IMG-20251015-WA0013.jpg')" }}
+      style={{ backgroundImage: "url('/WhatsApp Image 2025-10-20 at 2.44.25 PM.jpg')" }}
     >
       {/* Overlay oscuro para legibilidad */}
       <div className="absolute inset-0 bg-black/60 z-0"></div>
