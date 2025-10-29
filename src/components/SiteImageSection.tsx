17b6b04c0389807bda15c124466615d04f71ad3a
[dyad] <manjarrez.84@gmail.com>
2025-10-29 13:51:07 -0600

    [dyad] Cambiando la imagen de sitio2.png a sitio4.png en SiteImageSection. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/SiteImageSection.tsx
+++ src/components/SiteImageSection.tsx
@@ -5,7 +5,7 @@ const SiteImageSection = () => {
     <section className="bg-background">
       <div className="container mx-auto px-4 flex justify-center">
         <img 
-          src="/sitio2.png" 
+          src="/sitio4.png" 
           alt="Imagen del sitio" 
           className="w-full max-w-6xl h-auto rounded-lg" 
         />
