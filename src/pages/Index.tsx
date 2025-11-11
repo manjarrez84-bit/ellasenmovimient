627b2f4dccd1987d45f079afa6ea09443edac7ad
[dyad] <manjarrez.84@gmail.com>
2025-11-11 17:15:53 -0600

    [dyad] Adding a Supabase status check component to the Index page - wrote 2 file(s) + extra files edited outside of Dyad

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,12 +7,16 @@ import CallToActionSection from '@/components/CallToActionSection';
 import ContactSection from '@/components/ContactSection';
 import AnimatedSection from '@/components/AnimatedSection';
 import LatestBlogPostsSection from '@/components/LatestBlogPostsSection';
+import SupabaseStatusCheck from '@/components/SupabaseStatusCheck'; // Importar el nuevo componente
 
 const Index = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow pt-20"> {/* Añadido pt-20 aquí */}
+        
+        <SupabaseStatusCheck /> {/* Mostrar el estado de Supabase aquí */}
+
         <HeroSection />
         
         {/* AnimatedSection removed as it was empty after AboutSection removal */}
